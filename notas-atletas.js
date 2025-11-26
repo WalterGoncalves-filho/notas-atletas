1002f16a1e053d2a4c39c9853d5543cd39b8a7ba
Walter Gonçalves B Filho <163660669+WalterGoncalves-filho@users.noreply.github.com>
2025-11-26 18:57:00 -0400

    Update notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -17,3 +17,20 @@ let atletas = [
  }
 ];
 
+atletas.forEach(atleta=>{
+    let notasOrdenadas  = [...atleta.notas].sort((a,b)=>a-b)
+    notasOrdenadas.pop()
+    notasOrdenadas.shift()
+    let soma = 0   
+    notasOrdenadas.forEach(nota=>{
+        soma += nota
+    })
+    atleta.media = soma / notasOrdenadas.length   
+})
+for (let i = 0; i < atletas.length; i++) {
+    console.log(`\nAtleta: ${atletas[i].nome}`)
+    console.log(`Notas Obtidas: ${atletas[i].notas}`)
+    console.log(`Média Válida: ${atletas[i].media.toFixed(6).replace('.', ',')}\n`);
+    console.log()
+}
+
